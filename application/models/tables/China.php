114991e3703d7e8295dafa920fc22e32e6dc226b
liuyi <liuyi@didichuxing.com>
2016-08-22 23:01:13 +0800

    全国

---
+++ application/models/tables/China.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by IntelliJ IDEA.
+ * User: liuyi
+ * Date: 16/8/14
+ * Time: 下午4:49
+ *
+ * * system load.
+ * @property CI_Input $input CI class
+ * @property CI_Loader $model CI class
+ * @property CI_Loader $helper CI class
+ * @property CI_Loader $load CI class
+ * @property CI_Config $config CI class
+ */
+class China extends FF_Tables
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->init_db(__CLASS__);
+        $this->fields = array(
+            'id' ,
+            'name',
+            'parentId',
+            'shortName',
+            'levelType',
+        );
+    }
+
+
+
+    public function get_by_id($id)
+    {
+        $where = array('id'=>$id);
+        return $this->get_one($where);
+    }
+
+    public function get_by_parentid($parentid)
+    {
+        $where = array('parentId'=>$parentid);
+        return $this->get_all($where);
+    }
+
+}
