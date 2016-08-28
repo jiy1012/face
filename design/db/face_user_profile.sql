1b48324ff972cc64cefd8be7c94b92a59c422bde
jiy1012 <jiy1012@163.com>
2016-08-28 20:47:23 +0800

    area

---
+++ design/db/face_user_profile.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE  IF NOT EXISTS `face` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `face`;
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: face
+-- ------------------------------------------------------
+-- Server version	5.7.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_profile`
+--
+
+DROP TABLE IF EXISTS `user_profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_profile` (
+  `uid` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'uid',
+  `nickname` varchar(128) NOT NULL DEFAULT '' COMMENT '昵称',
+  `head_img` varchar(128) NOT NULL DEFAULT '' COMMENT '头像url',
+  `twitter` varchar(512) NOT NULL DEFAULT '' COMMENT '说说',
+  `introduction` text NOT NULL COMMENT '个人简介',
+  `area` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '地区',
+  `profession` int(4) unsigned NOT NULL DEFAULT '0' COMMENT '行业',
+  `company` varchar(128) NOT NULL DEFAULT '' COMMENT '公司',
+  `job` varchar(128) NOT NULL DEFAULT '' COMMENT '职业',
+  `school` varchar(128) NOT NULL DEFAULT '' COMMENT '学校',
+  `major` varchar(56) NOT NULL DEFAULT '' COMMENT '专业',
+  `reg_ip` varchar(15) NOT NULL DEFAULT '' COMMENT '注册ip',
+  `login_ip` varchar(15) NOT NULL DEFAULT '' COMMENT '登录ip',
+  `createtime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `updatetime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+LOCK TABLES `user_profile` WRITE;
+/*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-08-28 20:30:43
