3cf71cf81a51245c7a90d92a88a85adfa298f31e
jiy1012 <jiy1012@163.com>
2016-08-28 20:13:19 +0800

    area

--- application/helpers/arraytool_helper.php
+++ application/helpers/arraytool_helper.php
@@ -6,16 +6,23 @@
  * Date: 16/8/28
  * Time: 下午4:36
  */
-function format_array_parent_son($array,$primary_key = 'id',$parent_key = 'parentid',$son_key = 'data')
+function format_array_parent_son($array,$primary_key = 'id',$parent_key = 'parentId',$son_key = 'sons')
 {
     if (!is_array($array)){
         return array();
     }
+    $maps = array();
+    foreach ($array as $item) {
+        $maps[$item[$primary_key]] = $item;
+    }
     $res = array();
-    foreach ($array as $k=>$v) {
-        $res[$v[$parent_key]][$son_key][] = $v;
-        !isset($res[$v[$primary_key]]) && $res[$v[$primary_key]] = array();
-        $res[$v[$primary_key]] = array_merge($res[$v[$primary_key]],$v);
+    foreach ($maps as $k=>$v) {
+        if (!empty($v[$parent_key])){
+            $maps[$v[$parent_key]][$son_key][] = &$maps[$v[$primary_key]];
+        }else{
+            $res[] = &$maps[$v[$primary_key]];
+        }
     }
     return $res;
 }
+
